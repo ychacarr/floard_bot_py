c1af61324fc13158b9178af76171025d83912e5f
Yan Yablonski <yanyablonski@gmail.com>
2022-12-05 15:28:02 +0700

    Add congrats_from_yandex function.

--- congratulations.py
+++ congratulations.py
@@ -2,64 +2,86 @@ from async_scheduler import Job, Periods
 from database import Member
 import globals
 from datetime import datetime, timedelta
-import aiohttp
+from aiohttp import ClientSession
+from aiohttp.web import HTTPException
 import logging
 from random import randint
 
 log = logging.getLogger('congratulations_module')
 
-async def congrats_from_yandex(name: str) -> str:
-    # Текст поздравления слишком сильно завязан на 8 марта и в целом на поздравлении лица женского рода. "Чистить" текст от 8 марта слишком запаристо.
-    # Поэтому эта функция не используется в коде, возможно в будущем её можно будет приспособить для генерации поздравления с 8 марта.
-    exclude_words = {'весенний': '', 
-                    'Весенний': '',
-                    '8 марта': 'праздник',
-                    '8 Марта': 'праздник',
-                    'C 8 марта': 'С днём рождения',
-                    'С международным женским днем': 'С днем рождения',
-                    'Международный женский день': 'день рождения',
-                    'восьмое марта': 'празднество'}
-    url = 'https://yandex.ru/lab/api/postcard?name=' + name
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            data = await resp.json()
-            congrats_text = data.get('text')
-            for key in exclude_words:
-                if key in congrats_text:
-                    congrats_text = congrats_text.replace(key, exclude_words[key])
-            return congrats_text
+# async def congrats_from_yandex(name: str) -> str:
+#     # Текст поздравления слишком сильно завязан на 8 марта и в целом на поздравлении лица женского рода. "Чистить" текст от 8 марта слишком запаристо.
+#     # Поэтому эта функция не используется в коде, возможно в будущем её можно будет приспособить для генерации поздравления с 8 марта.
+#     exclude_words = {'весенний': '', 
+#                     'Весенний': '',
+#                     '8 марта': 'праздник',
+#                     '8 Марта': 'праздник',
+#                     'C 8 марта': 'С днём рождения',
+#                     'С международным женским днем': 'С днем рождения',
+#                     'Международный женский день': 'день рождения',
+#                     'восьмое марта': 'празднество'}
+#     url = 'https://yandex.ru/lab/api/postcard?name=' + name
+#     async with ClientSession() as session:
+#         async with session.get(url) as resp:
+#             data = await resp.json()
+#             congrats_text = data.get('text')
+#             for key in exclude_words:
+#                 if key in congrats_text:
+#                     congrats_text = congrats_text.replace(key, exclude_words[key])
+#             return congrats_text
 
-async def congrats_from_porfirii(name: str) -> str:
+# async def congrats_from_porfirii(name: str) -> str:
+#     """
+#     Функция обращается к API сайта https://porfirevich.ru для генерации текста поздравления.
+
+#     name -- имя именинника\n
+#     """
+#     session_header = {  'Connection': 'keep-alive', 
+#                         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+#                         'Content-Type': 'text/plain;charset=UTF-8',
+#                         'Accept': '*/*',
+#                         'Origin': 'https://porfirevich.ru',
+#                         'Sec-Fetch-Site': 'croos-site',
+#                         'Sec-Fetch-Mode': 'cors',
+#                         'Sec-Fetch-Dest': 'empty',
+#                         'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
+#                     }
+#     congrats_text = f'{name}' + '! Поздравляю тебя с днём рождения! Желаю, как и всегда, здоровья, счастья и'
+#     post_payload = ('{\"prompt\": \"' + congrats_text + '\", \"length\": 50}').encode(encoding='utf-8')
+#     log.info('Trying to connect to porfirii API...')
+#     async with ClientSession(headers=session_header) as session:
+#         async with session.post('https://pelevin.gpt.dobro.ai/generate/', data=post_payload) as resp:
+#             if resp.status == 200:
+#                 log.info('Connection to porfirii API successfull. Generating congratulations.')
+#                 json_data = await resp.json()
+#                 replies_list = json_data.get('replies')
+#                 replies_list.sort(key=lambda str_item: len(str_item), reverse=True)
+#                 congrats_text += replies_list[0]
+#                 return congrats_text
+#             else:
+#                 log.error(f'Connection to porfirii failed! Response status: {resp.status}.')
+#                 raise Exception('Response status != 200. Can\'t generate congratulations!')
+
+async def congrats_from_yandex(name: str) -> str:
     """
-    Функция обращается к API сайта https://porfirevich.ru для генерации текста поздравления.
+    Функция обращается к API Yandex Балабобы для генерации текста поздравления.
 
     name -- имя именинника\n
     """
-    session_header = {  'Connection': 'keep-alive', 
-                        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
-                        'Content-Type': 'text/plain;charset=UTF-8',
-                        'Accept': '*/*',
-                        'Origin': 'https://porfirevich.ru',
-                        'Sec-Fetch-Site': 'croos-site',
-                        'Sec-Fetch-Mode': 'cors',
-                        'Sec-Fetch-Dest': 'empty',
-                        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
-                    }
-    congrats_text = f'{name}' + '! Поздравляю тебя с днём рождения! Желаю, как и всегда, здоровья, счастья и'
-    post_payload = ('{\"prompt\": \"' + congrats_text + '\", \"length\": 50}').encode(encoding='utf-8')
-    log.info('Trying to connect to porfirii API...')
-    async with aiohttp.ClientSession(headers=session_header) as session:
-        async with session.post('https://pelevin.gpt.dobro.ai/generate/', data=post_payload) as resp:
+    congrats_text = f'{name}' + '! Поздравляю тебя с днём рождения! Желаю, как и всегда, здоровья, счастья и '
+    post_payload = {"query": congrats_text, "intro": 0, "filter": 1}
+    log.info('Trying to connect to Balaboba API...')
+    async with ClientSession() as session:
+        async with session.post('https://yandex.ru/lab/api/yalm/text3', json= post_payload) as resp:
             if resp.status == 200:
-                log.info('Connection to porfirii API successfull. Generating congratulations.')
+                log.info('Connection to Balaboba API successfull. Generating congratulations.')
                 json_data = await resp.json()
-                replies_list = json_data.get('replies')
-                replies_list.sort(key=lambda str_item: len(str_item), reverse=True)
-                congrats_text += replies_list[0]
+                balaboba_congrats = json_data["text"]
+                congrats_text += balaboba_congrats
                 return congrats_text
             else:
-                log.error(f'Connection to porfirii failed! Response status: {resp.status}.')
-                raise Exception('Response status != 200. Can\'t generate congratulations!')
+                raise HTTPException(status= resp.status, reason= "Connection to Balaboba failed!")   
+
 
 async def create_congrats(member_name: str) -> str:
     """
@@ -70,9 +92,9 @@ async def create_congrats(member_name: str) -> str:
     """
     congrats = f'{member_name}' + '! Поздравляю тебя с днём рождения! Желаю, как и всегда, здоровья, счастья и ... мои нейронные облака сломались. Поэтому просто всего!'
     try:
-        congrats = await congrats_from_porfirii(member_name)
-    except Exception:
-        pass
+        congrats = await congrats_from_yandex(member_name)
+    except HTTPException as ex:
+        log.error(f"{ex.reason} Response status: {ex.status}")
     finally:
         return congrats
 
